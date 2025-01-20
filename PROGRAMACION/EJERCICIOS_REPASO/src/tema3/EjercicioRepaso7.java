a46bd337595432af025eee7d8824af5d4ac9c948
Aitor1994 <amarin2512@ceuandalucia.es>
2025-01-20 14:06:45 +0100

    comit

---
+++ PROGRAMACION/EJERCICIOS_REPASO/src/tema3/EjercicioRepaso7.java
@@ -0,0 +1,19 @@
+package tema3;
+
+import java.util.Scanner;
+
+public class EjercicioRepaso7 {
+
+	public static void main(String[] args) {
+		/*Haz un programa que calcule el precio de un telegrama. Los telegramas se dictan normalmente, 
+          pero cambiando los puntos por la palabra “STOP”.  
+          El precio del telegrama se calcula multiplicando la cantidad total de palabras por un precio fijo 
+          (0,24 €). Obviamente, la palabra “STOP” no se cuenta. Ejemplo de ejecución: */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+
+	}
+
+}
---
+++ PROGRAMACION/TEMA3/src/Ejercicios_Repaso/Ejercicio10.java
@@ -0,0 +1,22 @@
+package Ejercicios_Repaso;
+
+
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		/*Haz un programa que pueda calcular la nota media de los alumnos de clase. Para ello, en primer 
+         lugar, el programa debe preguntar al usuario cuántos alumnos hay. A continuación, para cada 
+         alumno, solicitará su nombre y las notas de los 3 exámenes realizados. Después, una vez que 
+         haya preguntado todo, deberá mostrar un listado con las notas medias de cada alumno y la nota 
+         media de la clase.*/
+		
+		}
+		
+		
+		
+		
+
+	}
+
+
